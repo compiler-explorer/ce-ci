32ed8a71601eb187b19a4db45ed1f7632a9b7f0b
Matt Godbolt <matt@godbolt.org>
2023-08-28 21:08:03 -0500

    Bump all the terraform versions Just In case

--- providers.tf
+++ providers.tf
@@ -13,11 +13,11 @@ provider "random" {
 
 
 terraform {
-  required_version = "> 1.0.1"
+  required_version = "~> 1.5.6"
   required_providers {
     aws = {
       source = "hashicorp/aws",
-      version = "> 3.61.0"
+      version = "~> 5.2"
     }
   }
   backend "s3" {
