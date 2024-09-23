1f44d232523c6ac967dddd30e876e2514dcfeb7b
Partouf <partouf@gmail.com>
2024-09-23 18:00:48 +0200

    separate arm64 build

---
+++ build-image-arm64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TEMPLATE_FILE=terraform-aws-github-runner/images/ubuntu-focal/github_agent.ubuntu.pkr.hcl
+
+# REMEMBER TO UPDATE OUR PATCHED VERSION OF terraform-aws-github-runner
+
+packer init -upgrade -var-file=packer-vars-arm64.hcl "${TEMPLATE_FILE}"
+packer validate -var-file=packer-vars-arm64.hcl "${TEMPLATE_FILE}"
+packer build -var-file=packer-vars-arm64.hcl "${TEMPLATE_FILE}"
