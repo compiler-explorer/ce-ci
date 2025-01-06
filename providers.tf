1aa486343e8e779f56f58531c954226d452b5ada
Matt Godbolt <matt@godbolt.org>
2025-01-06 17:09:10 -0600

    Update terraform

--- providers.tf
+++ providers.tf
@@ -13,11 +13,11 @@ provider "random" {
 
 
 terraform {
-  required_version = "~> 1.8.0"
+  required_version = "~> 1.10.2"
   required_providers {
     aws = {
       source = "hashicorp/aws",
-      version = "~> 5.2"
+      version = "~> 5.82"
     }
   }
   backend "s3" {
